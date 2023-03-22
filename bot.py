d680d3a08e48e9e12cdb420b15d0a3a5dd89ac37
Rafael Nelson <rafaelson1337@gmail.com>
2023-03-22 12:00:15 -0300

    added feature to set a custom behavior and make prompts based on it

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import logging, os, openai
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters, PicklePersistence
 if not os.getenv("BOT_TOKEN") or not os.getenv("OPENAI_API_KEY"):
     from dotenv import load_dotenv
     load_dotenv() # for local testing purposes
@@ -23,16 +23,38 @@ async def question(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.sendMessage(chat_id=update.effective_chat.id, text=answer['choices'][0]['message']['content'])
 
 
+def set_custom_behavior(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.chat_data['custom_behavior'] = " ".join(context.args)
+
+async def custom_prompt(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    custom_behavior = context.chat_data.get('custom_behavior')
+
+    if custom_behavior:
+        question = " ".join(context.args)
+
+        answer = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[
+            {"role": "system", "content": custom_behavior},
+            {"role": "user", "content": question}
+        ])
+        await context.bot.sendMessage(chat_id=update.effective_chat.id, text=answer['choices'][0]['message']['content'])
+    else:
+        await context.bot.sendMessage(chat_id=update.effective_chat.id, text="Please set a custom behavior first.")
+
+
 async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="This command does not exist.")
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
+    chat_persistence = PicklePersistence(filepath="nelssistant_data")
+    application = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).persistence(chat_persistence).build()
 
     question_handler = CommandHandler('q', question)
-
+    set_custom_behavior_handler = CommandHandler('p', set_custom_behavior)
+    custom_prompt_handler = CommandHandler('c', custom_prompt)
     unknown_handler = MessageHandler(filters.COMMAND, unknown)
 
     application.add_handler(question_handler)
+    application.add_handler(set_custom_behavior_handler)
+    application.add_handler(custom_prompt_handler)
     application.add_handler(unknown_handler)
     application.run_polling()
